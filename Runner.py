54c4956d31690996d7af2800bbb2c25dbf5762d0
Niraj-kumbhar <kumbharniraj1@gmail.com>
2023-03-28 18:53:17 +0530

    added logs tracking to prechecks and runner

--- Runner.py
+++ Runner.py
@@ -1,2 +1,29 @@
 from scripts.prechecks import prechecks
 from scripts.email_main import email_main
+from datetime import datetime as dt
+
+dt_format = "%d/%m/%Y %H:%M:%S" # time format
+
+Slog =  open('logs/Scriptlogs.txt','a')
+Slog.write(f"\n\n{dt.now().strftime(dt_format)}-->Started, In Runner\n")
+Slog.close()
+
+pre = prechecks(1)
+pre.data()
+pre.error_check()
+pre.check_duplicates()
+err = pre.error # getting error flag
+
+Slog =  open('logs/Scriptlogs.txt','a')
+Slog.write(f"\n{dt.now().strftime(dt_format)}-->Prechecks completed\n")
+print("--"*10,"Prechecks completed","---"*10,"\n\n")
+
+if err == 1:
+    Slog.write(f"\n{dt.now().strftime(dt_format)}-->Error flag True\n")
+    opt = input("Above are some errors expected while sending mail, Do you still want to continue(y/n):")
+    if opt=="" or opt=='n':
+        Slog.write(f"\n{dt.now().strftime(dt_format)}-->User opt out, Exiting Script\n")
+        print("\n\n.....Exiting")
+        exit()
+    
+print("sending mails .....")
--- scripts/prechecks.py
+++ scripts/prechecks.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 from html.parser import HTMLParser
-from datetime import datetime
+from datetime import datetime as dt
   
 
 class prechecks:
@@ -10,16 +10,19 @@ class prechecks:
         try:
             if i==1: # passing arg to avoid auto run
                 os.chdir('scripts/')
+                self.dt_format = "%d/%m/%Y %H:%M:%S"
                 Slog =  open('..//logs/Scriptlogs.txt','a')
-                Slog.write("==*10"+f"\n\n{datetime.now()}-->Started Prechecks\n")
+                Slog.write(f"\n{dt.now().strftime(self.dt_format)}-->In Prechecks")
                 with open('../logs/reports.txt','w') as f:
                     f.write("="*10+"Prechecks Report"+"="*10+"\n")
-                    f.write(f"DateTime: {datetime.now()}\n")
+                    f.write(f"DateTime: {dt.now().strftime(self.dt_format)}\n")
                 
                 self.error = 0
-                self.data()
+                #self.data()
+                
+
         except Exception as e:
-            Slog.write(f"\n\n{datetime.now()}-->Exception occured\n")
+            Slog.write(f"\n\n{dt.now().strftime(self.dt_format)}-->Exception occured\n")
             print(e)
 
         finally:
@@ -29,7 +32,7 @@ class prechecks:
     def data(self):
         try:
             Slog =  open('..//logs/Scriptlogs.txt','a') # logs tracking
-            Slog.write(f"\n{datetime.now()}-->reading email_data file")
+            Slog.write(f"\n{dt.now().strftime(self.dt_format)}-->reading email_data file")
             self.df = pd.read_csv('..//email_config/email_data.csv')
             self.config = {}
             with open('..//email_config/config.txt','r') as f:
@@ -37,12 +40,12 @@ class prechecks:
                     line = line.strip()
                     name, var=line.partition("=")[::2]
                     self.config[name.strip()] = str(var).strip()
-            Slog.write(f"\n{datetime.now()}-->reading email_data file completed")
-            self.error_check()
+            Slog.write(f"\n{dt.now().strftime(self.dt_format)}-->reading email_data file completed")
+            #self.error_check()
         
         except Exception as e:
             print("Error in prechecks - data reading")
-            Slog.write(f"\n{datetime.now()}--> Error Occured {e}")
+            Slog.write(f"\n{dt.now().strftime(self.dt_format)}--> Error Occured {e}")
 
         finally:
             Slog.close()
@@ -51,7 +54,7 @@ class prechecks:
     def error_check(self):
         try:
             Slog =  open('..//logs/Scriptlogs.txt','a') # logs tracking
-            Slog.write(f"\n{datetime.now()}--> checking for errors..")
+            Slog.write(f"\n{dt.now().strftime(self.dt_format)}--> checking for errors..")
             for i in range(self.df.shape[0]):
                 file = f"{self.df.Name[i]}.{self.config['file_extension']}"
                 isExist = os.path.exists(f'..//data/{file}')
@@ -69,7 +72,7 @@ class prechecks:
                     f.write(f"Do you want to send one common file for all email(y/n): {temp}\n")
 
                 if isExist_con == False:
-                    Slog.write(f"\n{datetime.now()}--> No common file exists")
+                    Slog.write(f"\n{dt.now().strftime(self.dt_format)}--> No common file exists")
                     self.error = 1 # set Error flag true
                     with open('..//logs/reports.txt','a') as f:
                         f.write(f"..//data/{self.config['that_common_filename']} not exists\n")
@@ -88,14 +91,14 @@ class prechecks:
                     for i,_ in enumerate(list_error):
                         f.write(f"{i+1}.{_}\n")
             
-            self.check_duplicates()
-            Slog.write(f"\n{datetime.now()}--> checking for errors completed")
+            #self.check_duplicates()
+            Slog.write(f"\n{dt.now().strftime(self.dt_format)}--> checking for errors completed")
             with open('..//logs/reports.txt','r') as f:
                 print(f.read())
         
         except Exception as e:
             print("Error occured prechecks - error_check")
-            Slog.write(f"\n{datetime.now()}--> Error Occured {e}")
+            Slog.write(f"\n{dt.now().strftime(self.dt_format)}--> Error Occured {e}")
         
         finally:
             Slog.close()
@@ -103,7 +106,7 @@ class prechecks:
     def check_duplicates(self):
         try:
             Slog =  open('..//logs/Scriptlogs.txt','a') # logs tracking
-            Slog.write(f"\n{datetime.now()}--> checking for duplicates..")
+            Slog.write(f"\n{dt.now().strftime(self.dt_format)}--> checking for duplicates..")
             dup_name = self.df[self.df.Name.duplicated()]
           
             dup_email = self.df[self.df.Email.duplicated()]
@@ -120,7 +123,7 @@ class prechecks:
                 if dup_name.shape[0]>0:
                     self.error=1 # set Error flag true
                     f.write("\nDuplicate Name Data:\n")
-                    Slog.write(f"\n{datetime.now()}--> Duplicates found")
+                    Slog.write(f"\n{dt.now().strftime(self.dt_format)}--> Duplicates found")
                     for _ in dup_name.index:
                         name = dup_name.Name[_]
                         email = dup_name.Email[_]
@@ -133,10 +136,12 @@ class prechecks:
                         name = dup_email.Name[_]
                         email = dup_email.Email[_]
                         f.write(f"{name} {email}")
-             
+            print(self.error)
+            #return self.error 
+        
 
         except Exception as e:
             print(e)
         finally:
             Slog.close()
-            return self.error  
+             
